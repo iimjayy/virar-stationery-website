4820fb90822ac67e057f9060137c8173f71da4e7
Jaydeepsinh Jadeja <iimjayy21@gmail.com>
2026-06-20 17:59:59 +0530

    Refactor: clean up root directory architecture by removing 20+ throwaway AI scripts and unused CSS

--- fix_syntax.js
+++
@@ -1,5 +0,0 @@
-const fs = require('fs');
-let js = fs.readFileSync('assets/js/features/language-toggle.js', 'utf8');
-js = js.replace(/\\\\'/g, "\\'");
-fs.writeFileSync('assets/js/features/language-toggle.js', js);
-console.log('Fixed quotes.');
--- test_syntax.js
+++
@@ -1,3 +0,0 @@
-const x = {
-  'Walk in, collect your prints, and you\\'re done.': 'test'
-};
